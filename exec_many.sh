3c77c32b0efcad1083c3eb2b39b5d15e901c41ea
glener10 <glenerpizzolato@gmail.com>
2025-06-19 10:12:51 -0300

    feat: update prompt.txt to clarify criteria for identifying gaps and unaddressed points

--- exec_many.sh
+++ exec_many.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-python gemini.py -i "example.pdf" -m gemini-2.0-flash -o example_output.txt &
-python gemini.py -i "example.pdf" -m gemini-2.0-flash-lite -o example_output.txt &
-python gemini.py -i "example.pdf" -m gemini-1.5-flash -o example_output.txt &
-python gemini.py -i "example.pdf" -m gemini-2.5-flash-preview-05-20 -o example_output.txt &
+#gemini-2.0-flash
+# python3 gemini.py -i "1.pdf" -m gemini-2.0-flash -o outputs/a_responsabilidade_civil_das_instituicoes.txt &
+# python3 gemini.py -i "2.pdf" -m gemini-2.0-flash -o outputs/post_quantum_cryptography.txt &
+# python3 gemini.py -i "3.pdf" -m gemini-2.0-flash -o outputs/a_responsabilidade_das_instituicoes_financeiras_nas_fraudes.txt &
+# python3 gemini.py -i "4.pdf" -m gemini-2.0-flash -o outputs/tecnologia_de_pagamento_instantaneo_pix_criminalidades.txt &
+# python3 gemini.py -i "5.pdf" -m gemini-2.0-flash -o outputs/a_consumer_centric_approach.txt &
+# python3 gemini.py -i "6.pdf" -m gemini-2.0-flash -o outputs/cemlas.txt &
+# python3 gemini.py -i "7.pdf" -m gemini-2.0-flash -o outputs/central_bank.txt &
+
+#gemini-2.0-flash-preview
+python3 gemini.py -i "1.pdf" -m gemini-2.5-flash-preview-05-20 -o outputs/a_responsabilidade_civil_das_instituicoes.txt &
+python3 gemini.py -i "2.pdf" -m gemini-2.5-flash-preview-05-20 -o outputs/post_quantum_cryptography.txt &
+python3 gemini.py -i "3.pdf" -m gemini-2.5-flash-preview-05-20 -o outputs/a_responsabilidade_das_instituicoes_financeiras_nas_fraudes.txt &
+python3 gemini.py -i "4.pdf" -m gemini-2.5-flash-preview-05-20 -o outputs/tecnologia_de_pagamento_instantaneo_pix_criminalidades.txt &
+python3 gemini.py -i "5.pdf" -m gemini-2.5-flash-preview-05-20 -o outputs/a_consumer_centric_approach.txt &
+python3 gemini.py -i "6.pdf" -m gemini-2.5-flash-preview-05-20 -o outputs/cemlas.txt &
+python3 gemini.py -i "7.pdf" -m gemini-2.5-flash-preview-05-20 -o outputs/central_bank.txt &
 wait
